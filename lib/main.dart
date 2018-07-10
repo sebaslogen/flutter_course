3f9932f0ec2cff3fe508ece72949719e121e0e15
sebaslogen <sebastian@q42.nl>
2018-07-10 16:36:43 +0200

    :sparkles: First app with some static image card

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,35 @@
 import 'package:flutter/material.dart';
 
-//void main() => runApp();
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("first stage"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                child: Text("Add product"),
+                onPressed: () {},
+              ),
+            ),
+            Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset("assets/food.jpg"),
+                  Text("Food paradise")
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
