a0832180ea7b595b6d5a2b28b5b17041b935eea1
sebaslogen <sebastian@q42.nl>
2018-07-10 23:05:23 +0200

    Extrac widgets to separate classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,18 @@
 import 'package:flutter/material.dart';
 
+import './product_manager.dart';
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _MyAppSate();
-  }
-}
-
-class _MyAppSate extends State<MyApp> {
-  List<String> _products = ["Food tester"];
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("first stage"),
-        ),
-        body: Column(
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                child: Text("Add product"),
-                onPressed: () {
-                  setState(() {
-                    _products.add("new product");
-                  });
-                },
-              ),
-            ),
-            Column(
-              children: _products
-                  .map(
-                    (element) => Card(
-                          child: Column(
-                            children: <Widget>[
-                              Image.asset("assets/food.jpg"),
-                              Text(element)
-                            ],
-                          ),
-                        ),
-                  )
-                  .toList(),
-            ),
-          ],
-        ),
-      ),
+          appBar: AppBar(
+            title: Text("first stage"),
+          ),
+          body: ProductManager()),
     );
   }
 }
