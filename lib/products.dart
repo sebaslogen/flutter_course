2749bf0dc8f441e8f00df5507890eb915e1a8b49
sebaslogen <sebastian@q42.nl>
2018-08-07 10:45:48 +0200

    Add second screen with basic navigation

--- lib/products.dart
+++ lib/products.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_course/pages/product.dart';
 
 class Products extends StatelessWidget {
   final List<String> products;
@@ -30,7 +31,19 @@ class Products extends StatelessWidget {
       child: Column(
         children: <Widget>[
           Image.asset("assets/food.jpg"),
-          Text(products[index])
+          Text(products[index]),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                child: Text("Details"),
+                onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) => ProductPage())),
+              )
+            ],
+          )
         ],
       ),
     );
