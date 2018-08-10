b4a4c0545bedd01d304fcd1a412af9fc4a4acf39
sebaslogen <sebastian@q42.nl>
2018-08-10 21:28:44 +0200

    Use WillPopScope widget to control back button pop with extra data

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class ProductPage extends StatelessWidget {
@@ -8,26 +10,32 @@ class ProductPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(imageUrl),
-            Container(padding: EdgeInsets.all(10.0), child: Text(title)),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                child: Text("Back"),
-                color: Theme
-                    .of(context)
-                    .accentColor,
-                onPressed: () => Navigator.pop(context, true),
-              ),
-            )
-          ],
-        ));
+    return WillPopScope(
+      // Widget required to pass information back also on back-button
+      onWillPop: () {
+        Navigator.pop(context, false); // Manually pop passing our own data
+        return Future
+            .value(false); // Required but value is false to avoid a 2nd pop
+      },
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text(title),
+          ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(imageUrl),
+              Container(padding: EdgeInsets.all(10.0), child: Text(title)),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  child: Text("Back"),
+                  color: Theme.of(context).accentColor,
+                  onPressed: () => Navigator.pop(context, true),
+                ),
+              )
+            ],
+          )),
+    );
   }
 }
