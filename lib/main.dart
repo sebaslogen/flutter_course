1e1895087030441444815972a661ee7bee400fa0
sebaslogen <sebastian@q42.nl>
2018-08-14 15:16:29 +0200

    Use MaterialApp widget's router to navigate with default unknown navigation

--- lib/main.dart
+++ lib/main.dart
@@ -57,6 +57,11 @@ class _MyAppState extends State<MyApp> {
         }
         return null;
       },
+      onUnknownRoute: (RouteSettings settings) {
+        return MaterialPageRoute<bool>(
+            builder: (BuildContext context) =>
+                ProductsPage(_products, _addProduct, _deleteProduct));
+      },
     );
   }
 }
