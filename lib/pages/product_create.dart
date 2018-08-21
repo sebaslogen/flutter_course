d0c147eb7057011ee2daddf036463ac3a1de6cc5
sebaslogen <sebastian@q42.nl>
2018-08-21 22:15:59 +0200

    Add Bottom Sheet Modal

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -3,6 +3,14 @@ import 'package:flutter/material.dart';
 class ProductCreatePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Create a product"),);
+    return Center(
+        child: RaisedButton(
+            child: Text("Create a product"),
+            onPressed: () {
+              showModalBottomSheet<Widget>(
+                  context: context, builder: (BuildContext context) {
+                    return Center(child: Text("Modal"),);
+              });
+            }));
   }
 }
