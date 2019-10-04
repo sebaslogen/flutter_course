1e3999d3fae44c28c456a0bd9dd01ec1c2484a3a
sebaslogen <sebastian@q42.nl>
2019-10-04 16:49:01 +0200

    Add description and price to product details page

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -3,9 +3,11 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 class ProductPage extends StatelessWidget {
-  const ProductPage(this.title, this.imageUrl);
+  const ProductPage(this.title, this.description, this.price, this.imageUrl);
 
   final String title;
+  final String description;
+  final double price;
 
   final String imageUrl;
 
@@ -15,54 +17,49 @@ class ProductPage extends StatelessWidget {
       // Widget required to pass information back also on back-button
       onWillPop: () {
         Navigator.pop(context, false); // Manually pop passing our own data
-        return Future
-            .value(false); // Required but value is false to avoid a 2nd pop
+        return Future.value(
+            false); // Required but value is false to avoid a 2nd pop
       },
       child: Scaffold(
           appBar: AppBar(
             title: Text(title),
           ),
-          body: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Image.asset(imageUrl),
-              Container(padding: EdgeInsets.all(10.0), child: Text(title)),
-              Container(
-                padding: EdgeInsets.all(10.0),
-                child: RaisedButton(
-                  child: Text("Delete"),
-                  color: Theme.of(context).accentColor,
-                  onPressed: () => _showDialogWarning(context),
-                ),
-              )
-            ],
+          body: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(imageUrl),
+                Container(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(title,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                            fontSize: 26.0,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: 'Oswald'))),
+                Container(
+                    padding: const EdgeInsets.only(
+                        bottom: 10.0, left: 10.0, right: 10.0),
+                    child: Text(description,
+                        style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center)),
+                Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 6.0, vertical: 2.5),
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).accentColor,
+                        borderRadius: BorderRadius.circular(5.0)),
+                    child: Text(
+                      '\$' + price.toString(),
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(color: Colors.white),
+                    ))
+              ],
+            ),
           )),
     );
-  }
-
-  void _showDialogWarning(BuildContext context) {
-    showDialog<bool>(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text("Are you sure?"),
-            content: Text("This action can not be reverted"),
-            actions: <Widget>[
-              FlatButton(
-                child: Text("DISCARD"),
-                onPressed: () {
-                  Navigator.pop(context); // Closes dialog
-                },
-              ),
-              FlatButton(
-                child: Text("CONTINUE"),
-                onPressed: () {
-                  Navigator.pop(context); // Closes dialog
-                  Navigator.pop(context, true); // Closes screen with true
-                },
-              ),
-            ],
-          );
-        });
   }
 }
