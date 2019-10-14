97d340f0cf6e4b26f59d3757d9ffeda489255874
sebaslogen <sebastian@q42.nl>
2019-10-14 16:53:54 +0200

    Store image in app's directory

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
 
 class ImageInput extends StatefulWidget {
   @override
@@ -13,7 +15,13 @@ class _ImageInputState extends State<ImageInput> {
 
   Future<void> _takePicture() async {
     final imageFile =
-        ImagePicker.pickImage(source: ImageSource.camera, maxWidth: 600);
+        await ImagePicker.pickImage(source: ImageSource.camera, maxWidth: 600);
+    setState(() {
+      _storedImage = imageFile;
+    });
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(imageFile.path);
+    final savedImage = await imageFile.copy(path.join(appDir.path, fileName));
   }
 
   @override
