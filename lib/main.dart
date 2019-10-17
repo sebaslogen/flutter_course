a6b8acd8750c7b1a1517fd6eba3704f8de3f7721
sebaslogen <sebastian@q42.nl>
2019-10-17 11:52:44 +0200

    Clean up project

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_course/pages/auth.dart';
-import 'package:flutter_course/pages/product_admin.dart';
-import 'package:flutter_course/pages/product_page.dart';
-import 'package:flutter_course/pages/products_page.dart';
-//import 'package:flutter/rendering.dart'; // Required for debug paint
 
 void main() {
-//  debugPaintSizeEnabled = true; // Show sizes, margins and paddings
-//  debugPaintBaselinesEnabled = true; // Show text baselines
-//  debugPaintPointersEnabled = true; // Show touch events
   runApp(MyApp());
 }
 
@@ -20,54 +12,11 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final List<Map<String, dynamic>> _products = [];
-
-  void _addProduct(Map<String, dynamic> newProduct) {
-    setState(() {
-      _products.add(newProduct);
-    });
-  }
-
-  void _deleteProduct(int index) {
-    setState(() {
-      _products.removeAt(index);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
           primarySwatch: Colors.deepOrange, accentColor: Colors.deepPurple),
-//      home: AuthPage(), // This can not work while there is a route with root '/'
-      routes: {
-        '/': (BuildContext context) => AuthPage(),
-        '/products': (BuildContext context) => ProductsPage(_products),
-        '/admin': (BuildContext context) =>
-            ProductAdminPage(_addProduct, _deleteProduct),
-      },
-      onGenerateRoute: (RouteSettings settings) {
-        // Only for paths not managed in routes
-        final List<String> pathElements = settings.name.split('/');
-        if (pathElements[0] != '') {
-          return null;
-        }
-        if (pathElements[1] == 'product') {
-          final int index = int.parse(pathElements[2]);
-          return MaterialPageRoute<bool>(
-            builder: (BuildContext context) => ProductPage(
-                _products[index]['title'],
-                _products[index]['description'],
-                _products[index]['price'],
-                _products[index]['image']),
-          );
-        }
-        return null;
-      },
-      onUnknownRoute: (RouteSettings settings) {
-        return MaterialPageRoute<bool>(
-            builder: (BuildContext context) => ProductsPage(_products));
-      },
     );
   }
 }
