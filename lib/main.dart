1da95e2f4f8263c38c15aae789739c7ef0b4b624
sebaslogen <sebastian@q42.nl>
2019-10-20 15:13:25 +0200

    Add Flare animation with Smart-Flare lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smart_flare/smart_flare.dart';
 
 void main() {
   runApp(App());
@@ -18,11 +19,34 @@ class App extends StatelessWidget {
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Animated menu'),
       ),
-      body: Container(),
+      backgroundColor: Colors.grey[700],
+      body: Container(
+        child: Align(
+          alignment: Alignment.centerRight,
+          child: PanFlareActor(
+            width: 125.0,
+            height: screenSize.height,
+            filename: 'assets/slide-menu.flr',
+            openAnimation: 'open',
+            closeAnimation: 'close',
+            direction: ActorAdvancingDirection.RightToLeft,
+            threshold: 20.0,
+            reverseOnRelease: true,
+            completeOnThresholdReached: true,
+            activeAreas: [
+              RelativePanArea(
+                area: const Rect.fromLTWH(0, 0.7, 1.0, 0.3),
+//              debugArea: true,
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
